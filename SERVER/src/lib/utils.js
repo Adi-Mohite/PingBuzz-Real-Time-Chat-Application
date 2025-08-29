eddb7304ae67472c3058622cbee8bd555f4fea30
Adarsh Mohite <mohiteadarsh15@gmail.com>
2025-08-29 10:54:17 +0530

    Update utils.js

--- SERVER/src/lib/utils.js
+++ SERVER/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken= (userId,res)=>{
     res.cookie("jwt",Token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // miliseconds to 7 days
         httpOnly: true, //prevent xss attack cross-site scripting attacks
-        sameSite: "strict", //CSRF attack cross-site request forgery attacks
+        sameSite: "none", //CSRF attack cross-site request forgery attacks
         secure: process.env.NODE_ENV !=="development",
     });
 
