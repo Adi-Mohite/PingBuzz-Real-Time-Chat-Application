0038ef0c46847b208ceaa211c9b9267517bfdb81
Adarsh Mohite <mohiteadarsh15@gmail.com>
2025-08-29 09:44:14 +0530

    Update axios.js

--- CLIENT/src/lib/axios.js
+++ CLIENT/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL: "https://pingbuzz.vercel.app/api",
+    baseURL: "https://pingbuzz.onrender.com/api",
     withCredentials:true,
 })
