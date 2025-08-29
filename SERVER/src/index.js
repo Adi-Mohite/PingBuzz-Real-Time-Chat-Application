0c1b826ec45d417941836c9e372daf47d37e2989
Adarsh Mohite <mohiteadarsh15@gmail.com>
2025-08-29 10:50:26 +0530

    Update index.js

--- SERVER/src/index.js
+++ SERVER/src/index.js
@@ -18,6 +18,7 @@ const allowedOrigins = [
   "https://pingbuzz.vercel.app"
 ];
 app.use(cors({
+origin:allowedOrigins,
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
